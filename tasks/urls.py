c0fec717ea6e097ff6e8664994a911d0034c8caa
Anatoliy <anatoliy.klimanov@gmail.com>
2024-11-19 11:13:04 +0300

    2 шаг

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,9 @@ from tasks import views
 
 urlpatterns = [
     path('', views.index),
+    path('users/', views.users),
+    path('login/', views.login),
+    path('users/create/', views.create),
+    # http://127.0.0.1:8000/users/create/
     
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'tasks/index.html')
+
+
+def users(request):
+    return render(request, 'tasks/users.html')
+
+
+def login(request):
+    return render(request, 'tasks/login.html')
+
+
+def create(request):
+    return render(request, 'tasks/create.html')
