65a74833c851eb6608c744dbd65de99090c9b922
Anatoliy <anatoliy.klimanov@gmail.com>
2024-12-11 16:56:34 +0300

    попытка подключиться к рендеру

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-8gr#!_^1ojt_2rx#$0^*#ktn4!^nohu@34s&dh!dx!x=r-qjc6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost', 'python-project-52-blqo.onrender.com']
 
 
 # Application definition
