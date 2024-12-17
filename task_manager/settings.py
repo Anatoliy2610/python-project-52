3244007ce3fb60cb67d61cfef5dfb8d233938856
Anatoliy <anatoliy.klimanov@gmail.com>
2024-12-17 14:17:52 +0300

    настройка базы данных

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -96,10 +96,21 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': dj_database_url.config('DATABASES'),
+# }
+# print(dj_database_url.config('DATABASES'))
+
+
 DATABASES = {
-    'default': dj_database_url.config('DATABASES'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
