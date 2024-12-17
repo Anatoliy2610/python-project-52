1c5728e177eeb1d15f9f377e864b2c9338e8f86c
Anatoliy <anatoliy.klimanov@gmail.com>
2024-12-17 14:29:38 +0300

    изменение флеш сообщения после регистрации

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -19,7 +19,7 @@ class UsersCreate(SuccessMessageMixin,
     form_class = RegisterUserForm
     model = User
     template_name = 'actions/create_or_update.html'
-    success_message = ('Пользователь успешно создан')
+    success_message = ('Пользователь успешно зарегистрирован')
     success_url = reverse_lazy('login')
     extra_context = {
         'title': 'Регистрация',
